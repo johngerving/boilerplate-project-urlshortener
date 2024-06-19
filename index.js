2b1d81fb8633c5511ed2787b5858daa604d7e685
johngerving <gervingjohn@gmail.com>
2024-06-19 14:31:21 -0700

    Added createURL for inserting URL into database

--- index.js
+++ index.js
@@ -6,6 +6,40 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI);
 
+// Schema for storing original and shortened URLs
+let urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number,
+});
+
+let URL = mongoose.model("URL", urlSchema);
+
+const createURL = (original_url, done) => {
+  // Get current number of URL documents
+  URL.countDocuments({}).then((count) => {
+    // Add a new URL to the collection with a short url of 1 more than the previous one
+    let url = new URL({
+      original_url: original_url,
+      short_url: count + 1,
+    });
+
+    // Save document to db
+    url
+      .save()
+      .then((doc) => {
+        // Select original and short URL attributes from doc and return
+        let data = {
+          original_url: doc.original_url,
+          short_url: doc.short_url,
+        };
+        done(null, data);
+      })
+      .catch((err) => {
+        done(err, null);
+      });
+  });
+};
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -21,6 +55,8 @@ app.get("/", function (req, res) {
 app.post("/api/shorturl", function (req, res) {
   // Get URL submitted in req
   let original_url = req.body.url;
+
+  createURL(original_url, function (err, data) {});
 });
 
 app.listen(port, function () {
