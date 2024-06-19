9143945adf9e729d78814e3283a50010ea051545
johngerving <gervingjohn@gmail.com>
2024-06-19 14:17:01 -0700

    Installed mongoose, started post request implementation

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ mongoose.connect(process.env.MONGO_URI);
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
@@ -17,9 +18,9 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.post("/api/shorturl", function (req, res) {
+  // Get URL submitted in req
+  let original_url = req.body.url;
 });
 
 app.listen(port, function () {
